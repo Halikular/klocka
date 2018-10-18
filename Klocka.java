648ddc219b1ffc67607642a566f638874eabc577
Adam Blomberg <halikular@icloud.com>
2018-10-18 12:08:33 +0200

    Initial commit

---
+++ Klocka.java
@@ -0,0 +1,12 @@
+// Uppgift 2.1 (Sida 55)
+
+import java.util.Calendar;
+import javax.swing.JOptionPane;
+
+public class Klocka {
+	public static void main(String[] arg) {
+		String datumTid = Calendar.getInstance().getTime().toString();
+		int tid = datumTid.indexOf(':'); // Letar första förekomsten av ":" som senare subtraheras med 2
+		JOptionPane.showMessageDialog(null,"Klockan är: " + datumTid.substring(tid - 2, tid + 5));
+	}
+}
